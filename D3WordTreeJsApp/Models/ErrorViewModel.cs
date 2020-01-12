46673b80fe956818c36b48b214a3cd58904511a2
Md. Shohag Mia <shohag.mia@datasoft-bd.com>
2020-01-12 11:29:03 +0600

    Add project files.

---
+++ D3WordTreeJsApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace D3WordTreeJsApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
